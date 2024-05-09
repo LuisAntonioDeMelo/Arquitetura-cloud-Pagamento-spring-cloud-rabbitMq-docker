9c8ca03fe67cb2a88a51fda01d309b1266b83a62
luis antonio <tyluis20@gmail.com>
2024-05-08 22:23:45 -0300

    gateway config

--- mscloudgateway/mscloudgateway/src/main/java/io/github/pagamento/ls/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/mscloudgateway/src/main/java/io/github/pagamento/ls/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,24 @@ package io.github.pagamento.ls.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	//loudbalancer
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.build();
 	}
 
 }
