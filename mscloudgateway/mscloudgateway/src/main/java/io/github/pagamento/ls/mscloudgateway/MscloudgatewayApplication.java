03e52771bba0046bb1a9aaeb97878983deffdb12
luis antonio <tyluis20@gmail.com>
2024-05-08 22:14:15 -0300

    add gateway

---
+++ mscloudgateway/mscloudgateway/src/main/java/io/github/pagamento/ls/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.pagamento.ls.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+}
---
+++ mscloudgateway/mscloudgateway/src/test/java/io/github/pagamento/ls/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pagamento.ls.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
