033486b725a97fc862593e5bfb1496ba80e0c8d4
luis antonio <tyluis20@gmail.com>
2024-06-10 18:22:23 -0300

    add avaliador

---
+++ msavaliadorcredito/src/main/java/com/msavaliadorcredito/AvaliadorDeCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.msavaliadorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorDeCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorDeCreditoApplication.class, args);
+	}
+
+}
---
+++ msavaliadorcredito/src/test/java/com/msavaliadorcredito/AvaliadorDeCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliadorDeCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
